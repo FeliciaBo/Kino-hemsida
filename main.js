3cf506e785567d917ed2c13e82c5852b0d405975
Christoffer Borg <christofferborg@me.com>
2025-12-12 18:54:16 +0100

    Fetch movie API from IMDb

---
+++ main.js
@@ -0,0 +1,14 @@
+const getMoviePoster = async (imdbId) => {
+  const url = `http://www.omdbapi.com/?i=${imdbId}&apikey=58664eb2`;
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+  let container = document.querySelector('.poster-container');
+  let newPoster = document.createElement('div');
+  newPoster.classList.add('movie-poster');
+  newPoster.innerHTML = `<h2>${data.Title}</h2> <img src="${data.Poster}"</img>`;
+  container.appendChild(newPoster);
+}
+
+const ourMovies = ['tt0076759', 'tt0107290', 'tt0034583'];
+ourMovies.forEach(id => getMoviePoster(id));
